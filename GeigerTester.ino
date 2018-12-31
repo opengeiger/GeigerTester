852bbcff546c48758575577f6e7fd7d38092a62d
opengeiger <46202270+opengeiger@users.noreply.github.com>
2018-12-31 10:20:34 +0100

    Add files via upload

---
+++ GeigerTester.ino
@@ -0,0 +1,12 @@
+void setup() {
+  pinMode(8, OUTPUT);      // sets the digital pin as output
+}
+
+void loop() {
+  float cpm = 1200;
+  int del = round(1/(cpm/60.0)*1000/2);
+  digitalWrite(8, LOW);
+  delay(del);
+  digitalWrite(8, HIGH);
+  delay(del);
+}
